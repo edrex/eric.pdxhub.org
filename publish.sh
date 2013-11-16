4db3425f24e089ae4ba01cabc358797600e75358
Eric Drechsel <eric@pdxhub.org>
2013-11-15 22:35:44 -0800

    fix publish script for files with spaces
    boo bash

--- publish.sh
+++ publish.sh
@@ -1,8 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 
 harp compile ./ ./out
-for f in `ls out/*.html out/**/*.html`; do mv $f "${f%%.*}"; done
+
+IFS=$(echo -en "\n\b")
+
+for f in $(find out -name \*.html)
+  do mv "$f" "${f%%.*}"
+done
+
 # requires patched s3cmd from github
 # https://github.com/s3tools/s3cmd/issues/243
-
 s3cmd sync --default-mime-type="text/html; charset=utf-8" --guess-mime-type --delete-removed out/ s3://eric.pdxhub.org/
