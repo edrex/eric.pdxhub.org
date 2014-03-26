bbe08b129eabaa734a21fa61b19bb775f7fee6d2
Eric Drechsel <eric@pdxhub.org>
2014-03-26 13:43:33 -0700

    sass: bourbon

--- public/assets/_sass/config.rb
+++ public/assets/_sass/config.rb
@@ -1,7 +1,5 @@
 # Require any additional compass plugins here.
-require 'singularitygs'
-require 'modular-scale'
-require 'breakpoint'
+require 'bourbon'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
