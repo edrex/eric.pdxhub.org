f961f4beee71dc676f9b4d49f8d4d79c3d85612a
Eric Drechsel <eric@pdxhub.org>
2014-05-01 12:27:35 -0700

    contentEditable search box!

---
+++ public/assets/scripts/searchbox.js
@@ -0,0 +1,32 @@
+      function mountSearch(search) {
+          console.log('baz');
+          console.log(search);
+          var input = search.children('.input');
+          var form = search.children('form');
+          var query = form.children('[name=q]');
+          var lastHtml = input.html();
+
+          function copyInput () {
+              query.val(input.html());
+              return true;
+          }
+          function filterInput (){
+              var html = input.html();
+              if (html !== lastHtml) {
+                  console.log('filter');
+                  input.html(html.replace(/(<([^>]+)>)/ig,""))
+              }
+              lastHtml = html;
+          };    
+          function handleKeypress (e) {
+              var code = e.keyCode || e.which;
+              if(code == 13) {
+                  form.submit();
+                  e.preventDefault();
+              }
+          }
+          form.submit(copyInput);
+          input.blur(filterInput);
+          input.on('input', filterInput);
+          input.keypress(handleKeypress);
+      }
